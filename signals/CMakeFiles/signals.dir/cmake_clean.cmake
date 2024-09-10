4773da469e05efe9df71f0f65588abaff9807125
Cyfarwydd <Cyfarw9dd@outlook.com>
2024-09-10 15:52:50 +0800

    使用cmake构建项目，可通过编译

---
+++ signals/CMakeFiles/signals.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/signals.dir/root/CodeField_C/tlpi/lib/alt_functions.c.o"
+  "CMakeFiles/signals.dir/root/CodeField_C/tlpi/lib/alt_functions.c.o.d"
+  "CMakeFiles/signals.dir/root/CodeField_C/tlpi/lib/error_functions.c.o"
+  "CMakeFiles/signals.dir/root/CodeField_C/tlpi/lib/error_functions.c.o.d"
+  "CMakeFiles/signals.dir/root/CodeField_C/tlpi/lib/get_num.c.o"
+  "CMakeFiles/signals.dir/root/CodeField_C/tlpi/lib/get_num.c.o.d"
+  "CMakeFiles/signals.dir/sig_receiver.c.o"
+  "CMakeFiles/signals.dir/sig_receiver.c.o.d"
+  "CMakeFiles/signals.dir/signal_functions.c.o"
+  "CMakeFiles/signals.dir/signal_functions.c.o.d"
+  "signals"
+  "signals.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/signals.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
