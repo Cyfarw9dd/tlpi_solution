6f112ac4377caf9e3b93d14565aec2c432f5fe15
Cyfarwydd <Cyfarw9dd@outlook.com>
2024-09-24 10:39:56 +0800

    feat: 完成5-5

--- file/file_test.c
+++ file/file_test.c
@@ -15,12 +15,21 @@ int main(int argc, char *argv[])
 {
     if (argc > 1)
     {
-        opt_fd = open(argv[1], O_WRONLY, 0777);
-        flag |= O_CREAT;
-        fcntl(opt_fd, F_SETFL, flag);
-        printf("Current status: If the file is not existed, it will create the file.\n");
-
-        write(opt_fd, temp_buffer, sizeof(temp_buffer));
+        opt_fd = open(argv[1], O_WRONLY, 0766);
+        if (-1 == opt_fd)
+        {
+            perror("Occurr an error when opening the file.\n");
+            exit(1);
+        }
+        off_t offset = lseek(opt_fd, 1000, SEEK_SET);
+        printf("Using the old file descriptor, the offset is %ld.\n", offset);
+        int new_fd = dup(opt_fd);
+        off_t new_offset = lseek(new_fd, 0, SEEK_CUR);
+        printf("Acquiring the offset through the new file descriptor, the value is %ld.\n", new_offset);
+        new_offset = lseek(new_fd, 2000, SEEK_SET);
+        printf("Changing the offset through the new file descriptor, the value is %ld.\n", new_offset);
+        offset = lseek(opt_fd, 0, SEEK_CUR);
+        printf("Acquiring the offset through the old file descriptor, the value is %ld.\n", offset);
     }
 
     return 0;
